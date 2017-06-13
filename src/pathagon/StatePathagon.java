dac290e5805aa242443b5237409222c9b2f34898
larmas <lucas.armas.7@gmail.com>
2017-06-12 22:37:53 -0300

    Implements statePathagon

--- src/pathagon/StatePathagon.java
+++ src/pathagon/StatePathagon.java
@@ -1,7 +1,62 @@
 package pathagon;
 
+import java.util.Arrays;
+
 import framework.AdversarySearchState;
 
 public class StatePathagon implements AdversarySearchState {
-	
+	private int tokensUser; // Player available tokens
+	private int tokensCPU; //CPU available tokens
+	private boolean max; //true==isMaxNode false==isMinNode
+	private int turn; // turn== 1 is turn of player // turn==2 is turn of CPU
+	private int[][] board; //Representations of the board
+
+	public StatePathagon(boolean maxX, int tokensX, int tokensY, int turnX, int[][] boardX){
+
+		max = maxX;
+		tokensUser = tokensX;
+		tokensCPU = tokensY;
+		turn = turnX;
+		board = boardX;
+
+	}
+
+	public boolean isMax(){
+
+		return max;
+
+	}
+
+	public int turn(){
+
+		return turn;
+
+	}
+
+	public int tokensUser(){
+
+		return tokensUser;
+
+	}
+
+	public int tokensCPU(){
+
+		return tokensUser;
+
+	}
+
+	public boolean equals(StatePathagon other){
+
+		boolean res = true;
+		if (this.max != other.max){ res = false;}
+		if (this.turn != other.turn){ res = false;}
+		if (this.tokensUser != other.tokensUser){ res = false;}
+		if (this.tokensCPU != other.tokensCPU){ res = false;}
+		for(int i=0; i<this.board.length; i++{
+			if(!Arrays.equals(this.board[i]other.board[i])){ res = false;}
+		}
+
+		return res;
+
+	}
 }
